b4147c4a6ff42cd65dc4a896b7728fde23db7c0b
thinker <33519948+thinkerMid@users.noreply.github.com>
2022-04-23 13:18:04 +0800

    Add files via upload

---
+++ detoursdll/detoursdll.cpp
@@ -0,0 +1,22 @@
+﻿// detoursdll.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "detoursdll.h"
+
+
+// 这是导出变量的一个示例
+DETOURSDLL_API int ndetoursdll=0;
+
+// 这是导出函数的一个示例。
+DETOURSDLL_API int fndetoursdll(void)
+{
+    return 0;
+}
+
+// 这是已导出类的构造函数。
+Cdetoursdll::Cdetoursdll()
+{
+    return;
+}
